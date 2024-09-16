973bce8126281ca8851e5aff0203b23c9e2d46f3
smatthewenglish <s.matthew.english@gmail.com>
2024-09-16 13:46:39 -0400

    init

---
+++ src/main/java/com/smatthewenglish/crud_app_react/CrudAppReactApplication.java
@@ -0,0 +1,13 @@
+package com.smatthewenglish.crud_app_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smatthewenglish/crud_app_react/CrudAppReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smatthewenglish.crud_app_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
